5141eb88559f3f28d5338baaf2aff8190f35b97e
Ryo Nakayama <nakayamadevelop@gmail.com>
2022-05-09 20:34:32 +0900

    コンテンツ説明テーブルを追加

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface menus {
+  icon: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  menulist: menus[] = [
+    {
+      icon: 'grid_view',
+      description:
+        'Visualize time series data. For example, line graphs, histograms, scatter plots, etc. can be displayed.',
+    },
+    {
+      icon: 'psychology',
+      description:
+        'Analyze time series data. Aggregate values by Tag and correlation coefficient matrices can be viewed.',
+    },
+    {
+      icon: 'description',
+      description:
+        'For each Tag, you can associate a recognizable description with the unit.',
+    },
+    {
+      icon: 'settings',
+      description: 'Various settings can be changed. You can also upload data.',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
